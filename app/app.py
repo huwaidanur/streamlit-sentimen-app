c7ff92f9c5adebc59f2fdcfc8c6ec59a24d01235
huwaidanur <huwaidanurasysyifa@gmail.com>
2024-02-13 08:43:21 +0700

    perbaiki

--- app/app.py
+++ app/app.py
@@ -103,7 +103,7 @@ if selected_option == "Overview":
         with col1:
             data['cleaned_tweet'] = data['cleaned_tweet'].astype(str)
             word_lengths = data['cleaned_tweet'].str.len()
-            st.write(word_lengths)
+            st.dataframe(word_lengths)
             # = '#24d6e3'
             fig = go.Figure(data=[go.Histogram(x=word_lengths)])
             
