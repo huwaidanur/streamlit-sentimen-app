ff6c17f2a20e7486c274bb0454052f3a7e0e29b9
huwaidanur <huwaidanurasysyifa@gmail.com>
2024-06-03 10:10:00 +0700

    update

--- data_app/Scripts/activate.bat
+++ data_app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Data Science - Sanbercode\belajar\twitter_sentimen\data_app
+set VIRTUAL_ENV=D:\Data Science - Sanbercode\belajar\Twitter_Sentimen_\data_app
 
 if not defined PROMPT set PROMPT=$P$G
 
